ecbdd9acc881915942c9e81d8f5f578b795963a1
julitom1 <yarit.villalobos@mail.escuelaing.edu.co>
2021-09-09 12:16:51 -0500

    arreglando

---
+++ src/main/java/edu/eci/userServiceApp/utils/Constants.java
@@ -0,0 +1,13 @@
+package edu.eci.userServiceApp.utils;
+
+public interface Constants {
+
+    String CLAIMS_ROLES_KEY="ada_roles";
+    int TOKEN_DURATION_MINUTES = 1440;
+    String COOKIE_NAME="ada-JWT";
+
+    String ADMIN_ROLES="ADMIN";
+    String USER_R0LE="USER";
+
+
+}
